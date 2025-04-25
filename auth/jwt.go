585e24ce64cc51b2ead9811ded229c7561736e3c
NgigiN <ngigi.nyongo@gmail.com>
2025-04-25 22:09:26 +0300

    create: Implemented auth

--- auth/jwt.go
+++ auth/jwt.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/golang-jwt/jwt"
 )
 
@@ -25,4 +27,23 @@ func CreateJWT(secret []byte, email string) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+// HashPassword hashes a plain text password using bcrypt.
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// ComparePassword compares a hashed password with a plain text password.
+func ComparePassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
